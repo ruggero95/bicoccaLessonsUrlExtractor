cba56dcba433771bd79fd33d197f3d57dc482ad9
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-14 15:58:54 +0200

    fix parameter in get

--- src/app/bicocca/bicocca.controller.js
+++ src/app/bicocca/bicocca.controller.js
@@ -3,20 +3,22 @@ const { SuccessResponse, BadRequestResponse } = require('../../core/response')
 const BicoccaService = require('./bicocca.service')
 const router = express.Router()
 
-router.get('/get-course', async (req, res, next)=>{
-    try{
-        const fileName = req.params.filename
-        if(!fileName){
+router.get('/get-course', async (req, res, next) => {
+    try {
+        const { filename } = req.query
+        if (!filename) {
             return new BadRequestResponse('Missing filename').send(res)
         }
         const bicoccaService = new BicoccaService()
-        const exist = bicoccaService.checkFilePresence(fileName)
-        if(!exist){
+        const exist = bicoccaService.checkFilePresence(filename)
+        if (!exist) {
             return new BadRequestResponse('Missing file requested').send(res)
         }
-        const {zipPath, zipName} = await bicoccaService.zipContent(fileName)
-        return new SuccessResponse('',zipPath+zipName).download(res)
-    }catch(e){
+        const { zipPath, zipName } = await bicoccaService.zipContent(filename)
+        return new SuccessResponse('', zipPath + zipName).download(res)
+    } catch (e) {
         next(e)
     }
 })
+
+module.exports = router
