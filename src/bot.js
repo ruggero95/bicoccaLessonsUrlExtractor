70848a32577ca091d399748dfaba61a78084f158
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-14 11:36:44 +0200

    add check on username

--- src/bot.js
+++ src/bot.js
@@ -3,11 +3,17 @@ require('dotenv').config()
 const { start, commands, linkCourse, linkLesson, getLinkLessonScene, getLinkCourseScene, linkCourseScene, linkLessonScene } = require('./app/telegram')
 
 const bot = new Telegraf(process.env.API_TOKEN || '');
+bot.use((ctx,next)=>{
+	if (ctx.message.from.username != process.env.TELEGRAM_USERNAME) {
+		return ctx.reply('Not authorized, deploy your own bot, follow readme instruction')
+	}
+	return next()
+})
 
 bot.start((ctx) => commands(ctx))
 //app.command('a', )
 const stage = new Scenes.Stage([getLinkLessonScene(), getLinkCourseScene()], {
-	ttl: 10,
+	ttl: 30,
 });
 bot.use(session());
 bot.use(stage.middleware());
