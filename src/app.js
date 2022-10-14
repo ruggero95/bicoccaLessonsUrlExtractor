95b9c4c71a16d974711bb97db6696a48b046e29a
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-14 11:14:40 +0200

    added express

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const {SuccessResponse} = require('./core/response')
+const app = express()
+const corsOptions = {
+    origin:true,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    exposeHeaders:['*','Content-Disposition']
+}
+
+app.use(helmet())
+app.use(cors(corsOptions))
+app.get('/',(req, res, next)=>{
+    return new SuccessResponse('Running 👍').send(res)
+})
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,38 +1,19 @@
-//const csBic = require('./app/puppeteerBicocca')
-//csBic.test()
+const bot = require('./bot')
+const app = require('./app')
 require('dotenv').config()
+const PORT = process.env.PORT || 4003
 process.on('uncaughtException', (err) => {
 	//todo add logger
    console.log('errore')
    console.log(err)    
 });
-//const telegram = require('./app/telegram')
 
-//telegram.init()
-
-//csBic.pageAnalizer('https://elearning.unimib.it/course/view.php?id=31208')
-//csBic.pageAnalizer('https://elearning.unimib.it/course/view.php?id=31198')
-const { Telegraf, Scenes, session } = require('telegraf')
-const { start, commands, linkCourse, linkLesson, getLinkLessonScene, getLinkCourseScene, linkCourseScene, linkLessonScene } = require('./app/telegram')
-
-const bot = new Telegraf(process.env.API_TOKEN || '');
-
-bot.start((ctx) => commands(ctx))
-//app.command('a', )
-const stage = new Scenes.Stage([getLinkLessonScene(), getLinkCourseScene()], {
-	ttl: 10,
-});
-bot.use(session());
-bot.use(stage.middleware());
-bot.use((ctx, next) => {
-	// we now have access to the the fields defined above
-	ctx.scene.session.mySceneSessionProp ??= 0;
-	return next();
-});
-bot.hears(linkLesson, (ctx)=> ctx.scene.enter(linkLessonScene))
-bot.hears(linkCourse, (ctx)=> ctx.scene.enter(linkCourseScene))
 bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+app.listen(PORT,()=>{
+    console.log(`Running on port ${PORT}`)
+})
